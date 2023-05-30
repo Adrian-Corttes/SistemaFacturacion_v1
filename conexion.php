2b12602e50928942c4ea18a8840b50bfd077c780
Adrian Cortes <ls-adrian@hotmail.com>
2023-05-30 12:11:15 -0500

    Initial commit

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $bd = "sistemafacturacion";
+
+    $conection = @mysqli_connect($host,$user,$password,$bd);
+    
+    if(!$conection){
+        echo "Error en la conexion";
+    }else{
+        echo "Conexion exitosa"; 
+    }
+?>
