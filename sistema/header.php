3f41051e019332835ea9429a102b40f14e049b13
Adrian Cortes <ls-adrian@hotmail.com>
2023-06-05 12:04:50 -0500

    Se crearon las plantillas para la sesión principal del sistema

---
+++ sistema/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="header">
+
+        <h1>Sistema Facturación V1.0</h1>
+        <div class="optionsBar">
+            <p>Medellin, 2 de Junio de 2023</p>
+            <span>|</span>
+            <span class="user">Adrian Cortes</span>
+            <img class="photouser" src="img/user.png" alt="Usuario">
+            <a href="salir.php"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
+        </div>
+    </div>
+    
+    <?php include  "./nav.php"?>
+</header>
